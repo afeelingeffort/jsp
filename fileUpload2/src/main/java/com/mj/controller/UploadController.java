cf58d5589324ba7af38192ff7668b3ea7236b1ba
afeelingeffort <kmg1151@naver.com>
2023-04-06 20:26:15 +0900

    수정

---
+++ fileUpload2/src/main/java/com/mj/controller/UploadController.java
@@ -0,0 +1,67 @@
+package com.mj.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet("/uploadProc")
+@MultipartConfig
+public class UploadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UploadController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Part filePart = request.getPart("file");
+
+		InputStream fileContent = filePart.getInputStream();
+		OutputStream outputStream = null;
+
+		try {
+			UUID uuid = UUID.randomUUID();
+			String fileName = uuid + "_" + filePart.getSubmittedFileName();
+
+			File file = new File("C:/fileUpload2/", fileName);
+
+			outputStream = new FileOutputStream(file);
+
+			byte[] buffer = new byte[1024];
+			int length;
+
+			while ((length = fileContent.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, length);
+			}
+			System.out.println("파일 생성 완료");
+		} catch (Exception e) {
+
+		} finally {
+			fileContent.close();
+			if(outputStream != null) {
+				outputStream.flush();
+				outputStream.close();
+			}
+			
+			response.sendRedirect("/fileUpload2/home.jsp");
+		}
+
+	}
+
+}
